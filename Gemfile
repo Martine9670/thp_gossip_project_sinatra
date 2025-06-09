3ebb38691b5419c6b62df33f2d8d9adb4d7279c4
Martine9670 <pinnamartine@free.fr>
2025-06-09 20:37:55 +0200

    add all

---
+++ Gemfile
@@ -0,0 +1,8 @@
+gem 'sinatra'
+
+gem "rackup", "~> 2.2"
+gem "puma", "~> 6.6"
+
+gem "rerun", "~> 0.14.0"
+
+gem "csv", "~> 3.3"
---
+++ lib/controller.rb
@@ -0,0 +1,15 @@
+require_relative 'gossip'
+
+class ApplicationController < Sinatra::Base
+    get '/' do
+        erb :index
+    end
+
+    get '/gossips/new/' do
+        erb :new_gossip
+    end
+
+    post '/gossips/new/' do
+        Gossip.new.save
+    end
+end
