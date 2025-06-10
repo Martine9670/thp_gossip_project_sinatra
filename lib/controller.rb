da7acaad357c8b67488c652d383e66d84916cd64
Martine9670 <pinnamartine@free.fr>
2025-06-10 10:24:35 +0200

    add all

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,7 +2,7 @@ require_relative 'gossip'
 
 class ApplicationController < Sinatra::Base
     get '/' do
-        erb :index
+        erb :index, locals: {gossips: Gossip.all}
     end
 
     get '/gossips/new/' do
@@ -10,6 +10,15 @@ class ApplicationController < Sinatra::Base
     end
 
     post '/gossips/new/' do
-        Gossip.new("super_auteur", "super_gossip").save
+        puts "Salut, je suis dans le serveur"
+        puts "Ceci est le contenu du hash params : #{params}"
+        puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+        puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+        puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+
+        Gossip.new(params["gossip_author"], params["gossip_content"]).save
+
+        redirect '/'
     end
+
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,17 +1,25 @@
 require 'csv'
 
 class Gossip
-    attr_reader :author, :content
+    attr_reader :gossip_author, :gossip_content
 
-        def initialize(author, content)
-        @author = author
-        @content = content
+        def initialize(gossip_author, gossip_content)
+        @gossip_author = gossip_author
+        @gossip_content = gossip_content
         end
 
 
     def save
         CSV.open("./db/gossip.csv", "ab") do |csv|
-            csv << [author, content]
+            csv << [@gossip_author, @gossip_content]
         end
+    end
+
+    def self.all
+        all_gossips = [] # On initialise un array vide
+        CSV.read("./db/gossip.csv").each do |csv_line| # Va chercher chacune des lignes du csv do
+            all_gossips << Gossip.new(csv_line[0], csv_line[1]) # Crée un gossip avec les infos de la ligne
+        end
+        return all_gossips # Retourne un array rempli d'objets Gossip
     end
 end
